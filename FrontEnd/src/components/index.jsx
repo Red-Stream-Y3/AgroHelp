52277a3dd804c9e9ffc039d30a14a1f28092bfa9
Navod <54998118+navodveduth@users.noreply.github.com>
2023-04-30 10:54:31 +0530

    implement admin panel ui structure

--- FrontEnd/src/components/index.jsx
+++ FrontEnd/src/components/index.jsx
@@ -1,2 +1,5 @@
 export { default as Navbar } from './common/Navbar';
 export { default as Footer } from './common/Footer';
+
+export { default as AdminSideBar } from './admin/AdminSideBar';
+export { default as BoxWidget } from './admin/BoxWidget';
