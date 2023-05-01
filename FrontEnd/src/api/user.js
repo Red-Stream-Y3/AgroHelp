ef7b428ccb719ee666c9f281be362ddfeef7ca62
Navod <54998118+navodveduth@users.noreply.github.com>
2023-05-01 14:51:59 +0530

    frontend user api

---
+++ FrontEnd/src/api/user.js
@@ -0,0 +1,60 @@
+import axios from 'axios';
+
+// user login
+export const login = async (email, password) => {
+  try {
+    const response = await axios.post('http://localhost:9120/api/users/login', {
+      email,
+      password,
+    });
+    localStorage.setItem('userInfo', JSON.stringify(response.data));
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// user register
+export const register = async (
+  username,
+  firstName,
+  lastName,
+  email,
+  password
+) => {
+  try {
+    const response = await axios.post('http://localhost:9120/api/users', {
+      username,
+      firstName,
+      lastName,
+      email,
+      password,
+    });
+    localStorage.setItem('userInfo', JSON.stringify(response.data));
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// user logout
+export const logout = async () => {
+  localStorage.removeItem('userInfo');
+  document.location.href = '/';
+};
+
+// get users
+export const getUsers = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  try {
+    const response = await axios.get('http://localhost:9120/api/users', config);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
