0518a1a9ef718178335a84d8ebc440c34e6aafcc
Navod <54998118+navodveduth@users.noreply.github.com>
2023-05-01 14:54:08 +0530

    implement user login & register

--- FrontEnd/src/components/index.jsx
+++ FrontEnd/src/components/index.jsx
@@ -1,5 +1,8 @@
 export { default as Navbar } from './common/Navbar';
 export { default as Footer } from './common/Footer';
+export { default as Loader } from './common/Loader';
 
 export { default as AdminSideBar } from './admin/AdminSideBar';
 export { default as BoxWidget } from './admin/BoxWidget';
+export { default as BlogTable } from './admin/BlogTable';
+export { default as UserTable } from './admin/UserTable';
