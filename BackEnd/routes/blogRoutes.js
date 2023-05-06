460efd57fbb0225596d57a34cb98e3b12089b56c
Navod <54998118+navodveduth@users.noreply.github.com>
2023-05-06 14:41:47 +0530

    blog accept function

--- BackEnd/routes/blogRoutes.js
+++ BackEnd/routes/blogRoutes.js
@@ -1,10 +1,23 @@
 import express from 'express';
-import { createBlog, updateBlog, deleteBlog, searchBlogs, getBlogById, getBlogs, createBlogComment, deleteBlogComment, likeBlog, dislikeBlog} from '../controllers/blogController.js';
+import {
+  createBlog,
+  updateBlog,
+  deleteBlog,
+  searchBlogs,
+  getBlogById,
+  getBlogs,
+  createBlogComment,
+  deleteBlogComment,
+  likeBlog,
+  dislikeBlog,
+  updateBlogAccept,
+} from '../controllers/blogController.js';
 
 const router = express.Router();
 
 router.route('/').get(getBlogs).post(createBlog);
 router.route('/:id').get(getBlogById).put(updateBlog).delete(deleteBlog);
+router.route('/:id/accept').put(updateBlogAccept);
 router.route('/comment/:id').post(createBlogComment);
 router.route('/search/:blogTitle').get(searchBlogs);
 router.route('/comment/:id/:commentId').delete(deleteBlogComment);
