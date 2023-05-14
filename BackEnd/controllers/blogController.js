f9a1914c13df9ced06aeae224e9bea5b9c921959
Janindu Dissanayake <87414583+JaninduDissanyake@users.noreply.github.com>
2023-05-14 22:08:27 +0530

    latest 4 blogs controller

--- BackEnd/controllers/blogController.js
+++ BackEnd/controllers/blogController.js
@@ -300,6 +300,25 @@ const blogCommentAccept = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    GEt random 4 blogs
+// @route   GET /api/blog/random
+// @access  Public
+
+const getLatestBlogs = asyncHandler(async (req, res) => {
+  try {
+    const latestBlogPosts = await Blog.find().sort({createdAt: -1}).limit(4);
+    if (latestBlogPosts) {
+      res.status(200).json(latestBlogPosts);
+    } else {
+      throw new Error('No blogs found');
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ message: 'No blogs found' });
+  }
+});
+
+
 export {
   getBlogs,
   getBlogById,
@@ -316,4 +335,5 @@ export {
   blogCommentAccept,
   bookmarkBlog,
   getBookmarkedBlogs,
+  getLatestBlogs
 };
