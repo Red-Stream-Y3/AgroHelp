60aca278f8d8a92211ba99f2d231d05cbe5e8542
Janindu Dissanayake <87414583+JaninduDissanyake@users.noreply.github.com>
2023-05-14 21:49:25 +0530

    minor changes

--- FrontEnd/src/pages/blog/BlogSearch.jsx
+++ FrontEnd/src/pages/blog/BlogSearch.jsx
@@ -1,9 +1,9 @@
 /** @format */
 
-import { useState, useEffect } from 'react';
-import { PublicBlogCard, Loader } from '../../components';
-import { useParams } from 'react-router-dom';
-import { searchBlog } from '../../api/blog';
+import { useState, useEffect } from "react";
+import { PublicBlogCard, Loader, BlogContainer } from "../../components";
+import { useParams } from "react-router-dom";
+import { searchBlog } from "../../api/blog";
 
 const BlogSearch = () => {
   const { searchTerm } = useParams();
@@ -33,31 +33,33 @@ const BlogSearch = () => {
 
   return (
     <div>
-      <div className="flex flex-col items-center justify-center bg-gray-900">
-        <div className="flex flex-col items-center place-items-center py-5 px-8 ml-0 mr-auto w-full">
-          <h1 className="text-3xl text-left text-white font-bold pt-5 pl-5">
-            Search Results for : {searchTerm}
-          </h1>
+      <BlogContainer>
+        <div className="flex flex-col items-center justify-center bg-gray-900">
+          <div className="flex flex-col items-center place-items-center py-5 px-8 ml-0 mr-auto w-full">
+            <h1 className="text-3xl text-left text-white font-bold pt-5 pl-5">
+              Search Results for : {searchTerm}
+            </h1>
 
-          <hr className="border-gray-500 border-1 w-full mt-4" />
+            <hr className="border-gray-500 border-1 w-full mt-4" />
+          </div>
+          <div className="flex flex-wrap justify-center md:justify-center md:pl-3">
+            {searchResults.map((blog) => (
+              <div key={blog._id} className="m-4 w-full">
+                <PublicBlogCard
+                  title={blog.title}
+                  author={blog.author.firstName + " " + blog.author.lastName}
+                  authorID={blog.author._id}
+                  date={blog.createdAt}
+                  tags={blog.tags}
+                  likes={blog.likes.length}
+                  dislikes={blog.dislikes.length}
+                  comments={blog.comments.length}
+                />
+              </div>
+            ))}
+          </div>
         </div>
-        <div className="flex flex-wrap justify-center md:justify-center md:pl-3">
-          {searchResults.map((blog) => (
-            <div key={blog._id} className="m-4 w-full">
-              <PublicBlogCard
-                title={blog.title}
-                author={blog.author.firstName + ' ' + blog.author.lastName}
-                authorID={blog.author._id}
-                date={blog.createdAt}
-                tags={blog.tags}
-                likes={blog.likes.length}
-                dislikes={blog.dislikes.length}
-                comments={blog.comments.length}
-              />
-            </div>
-          ))}
-        </div>
-      </div>
+      </BlogContainer>
     </div>
   );
 };
