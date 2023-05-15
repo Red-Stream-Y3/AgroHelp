db661cad0bd4762a561806fb407f38187d4f3d51
IT20159030 <it20159030@my.sliit.lk>
2023-05-15 09:51:16 +0530

    HOME CHANGES

--- FrontEnd/src/pages/common/Home.jsx
+++ FrontEnd/src/pages/common/Home.jsx
@@ -1,5 +1,3 @@
-/** @format */
-
 import { useState, useEffect } from "react";
 import { getAllCropsShort, getRandomDiseases } from "../../api/knowlegdebase";
 import {
@@ -127,80 +125,94 @@ const Home = () => {
   }
 
   return (
-    <div>
-      <div className="flex flex-col items-center justify-center">
-        <div className="flex flex-col items-start place-items-start py-5 px-8 ml-0 mr-auto w-full">
-          <h1 className="text-2xl text-white font-bold md:text-3xl">
-            Featured Crops
-          </h1>
-          <p className="text-gray-300 text-md md:text-lg">
-            Learn about different crops and their growing conditions
-          </p>
-          <hr className="border-gray-500 border-1 w-full mt-4" />
-        </div>
-        {/* mobile carousal */}
-        <div className="relative">
-          <div className="flex flex-wrap justify-center md:hidden">
-            <div className="m-5">
-              <CropCard crop={crops[currentcrop]} style={animateStyle} />
-            </div>
-            <div className="absolute left-2 top-1/2 transform -translate-y-1/2">
-              <FaChevronCircleLeft
-                className="text-3xl text-white cursor-pointer"
-                onClick={handlePrevClickCrop}
-              />
-            </div>
-            <div className="absolute right-2 top-1/2 transform -translate-y-1/2">
-              <FaChevronCircleRight
-                className="text-3xl text-white cursor-pointer"
-                onClick={handleNextClickCrop}
-              />
-            </div>
+      <div>
+          <div className="flex flex-col items-center justify-center">
+              <div className="flex flex-col items-start place-items-start py-5 px-8 ml-0 mr-auto w-full">
+                  <h1 className="text-2xl text-white font-bold md:text-3xl">
+                      Featured Crops
+                  </h1>
+                  <p className="text-gray-300 text-md md:text-lg">
+                      Learn about different crops and their growing conditions
+                  </p>
+                  <hr className="border-gray-500 border-1 w-full mt-4" />
+              </div>
+              {/* mobile carousal */}
+              <div className="relative">
+                  <div className="flex flex-wrap justify-center md:hidden">
+                      <div className="m-5">
+                          <CropCard
+                              crop={crops[currentcrop]}
+                              style={animateStyle}
+                          />
+                      </div>
+                      <div className="absolute left-2 top-1/2 transform -translate-y-1/2">
+                          <FaChevronCircleLeft
+                              className="text-3xl text-white cursor-pointer"
+                              onClick={handlePrevClickCrop}
+                          />
+                      </div>
+                      <div className="absolute right-2 top-1/2 transform -translate-y-1/2">
+                          <FaChevronCircleRight
+                              className="text-3xl text-white cursor-pointer"
+                              onClick={handleNextClickCrop}
+                          />
+                      </div>
+                  </div>
+              </div>
+
+              {/* desktop */}
+              <div className="hidden md:flex flex-wrap justify-center">
+                  {crops.map((crop) => (
+                      <div className="m-4" key={crop._id}>
+                          <CropCard crop={crop} />
+                      </div>
+                  ))}
+              </div>
           </div>
-        </div>
 
-        {/* desktop */}
-        <div className="hidden md:flex flex-wrap justify-center">
-          {crops.map((crop) => (
-            <div className="m-4" key={crop._id}>
-              <CropCard crop={crop} />
-            </div>
-          ))}
-        </div>
-      </div>
+          <div className="flex flex-col items-center justify-center">
+              <div className="flex flex-col items-start place-items-start py-5 px-8 ml-0 mr-auto w-full">
+                  <h1 className="text-2xl text-white font-bold md:text-3xl">
+                      Featured Diseases
+                  </h1>
+                  <p className="text-gray-300 text-md md:text-lg">
+                      Learn about different diseases and their symptoms
+                  </p>
+                  <hr className="border-gray-500 border-1 w-full mt-4" />
+              </div>
+              {/* mobile carousal */}
+              <div className="relative">
+                  <div className="flex flex-wrap justify-center md:hidden">
+                      <div className="m-5">
+                          <DiseaseCard
+                              disease={diseases[currentdisease]}
+                              style={animateStyle}
+                          />
+                      </div>
+                      <div className="absolute left-2 top-1/2 transform -translate-y-1/2">
+                          <FaChevronCircleLeft
+                              className="text-3xl text-white cursor-pointer"
+                              onClick={handlePrevClickDisease}
+                          />
+                      </div>
+                      <div className="absolute right-2 top-1/2 transform -translate-y-1/2">
+                          <FaChevronCircleRight
+                              className="text-3xl text-white cursor-pointer"
+                              onClick={handleNextClickDisease}
+                          />
+                      </div>
+                  </div>
+              </div>
 
-
-      <div className="flex flex-col items-center justify-center">
-        <div className="flex flex-col items-start place-items-start py-5 px-8 ml-0 mr-auto w-full">
-          <h1 className="text-2xl text-white font-bold md:text-3xl">
-            Featured Diseases
-          </h1>
-          <p className="text-gray-300 text-md md:text-lg">
-            Learn about different diseases and their symptoms
-          </p>
-          <hr className="border-gray-500 border-1 w-full mt-4" />
-        </div>
-        {/* mobile carousal */}
-        <div className="relative">
-          <div className="flex flex-wrap justify-center md:hidden">
-            <div className="m-5">
-              <DiseaseCard
-                disease={diseases[currentdisease]}
-                style={animateStyle}
-              />
-            </div>
-            <div className="absolute left-2 top-1/2 transform -translate-y-1/2">
-              <FaChevronCircleLeft
-                className="text-3xl text-white cursor-pointer"
-                onClick={handlePrevClickDisease}
-              />
-            </div>
-            <div className="absolute right-2 top-1/2 transform -translate-y-1/2">
-              <FaChevronCircleRight
-                className="text-3xl text-white cursor-pointer"
-                onClick={handleNextClickDisease}
-              />
-            </div>
+              {/* desktop */}
+              <div className="hidden md:flex flex-wrap justify-center">
+                  {diseases.map((disease) => (
+                      <div className="m-4" key={disease._id}>
+                          <DiseaseCard disease={disease} />
+                      </div>
+                  ))}
+              </div>
+          </div>
 
           {/* latest forums */}
           <div className="flex flex-col items-center justify-center text-white mb-10">
@@ -227,79 +239,40 @@ const Home = () => {
                   )}
               </div>
           </div>
-        </div>
-
-        {/* desktop */}
-        <div className="hidden md:flex flex-wrap justify-center">
-          {diseases.map((disease) => (
-            <div className="m-4" key={disease._id}>
-              <DiseaseCard disease={disease} />
+          <div className="text-white p-5 sm:p-10">
+            <div>
+              <h1 className="text-2xl text-white font-bold md:text-3xl">
+                Latest Blogs
+              </h1>
+              <p className="text-gray-300 text-md md:text-lg">
+                Everything you need to know about Agriculture
+              </p>
+              <hr className="border-gray-500 border-1 w-full mt-4" />
             </div>
-          ))}
-        </div>
-      </div>
-
-      {/* latest forums */}
-      <div className="text-white p-5 sm:p-10">
-        <div>
-          <h1 className="text-2xl text-white font-bold md:text-3xl">
-            Latest Forums
-          </h1>
-          <p className="text-gray-300 text-md md:text-lg">
-            Ask questions and get answers from the community
-          </p>
-          <hr className="border-gray-500 border-1 w-full mt-4" />
-        </div>
-        <div>
-          {forumsLoading ? (
-            <Skeleton count={6} />
-          ) : (
-            <ForumCardContainer
-              forums={forums}
-              loaded={forumLoaded}
-              tab="home"
-              refresh={refreshForums}
-              refreshAll={() => setForumLoaded(false)}
-            />
-          )}
-        </div>
-      </div>
-
-      {/* latest blogs */}
-      <div className="text-white p-5 sm:p-10">
-        <div>
-          <h1 className="text-2xl text-white font-bold md:text-3xl">
-            Latest Blogs
-          </h1>
-          <p className="text-gray-300 text-md md:text-lg">
-            Everything you need to know about Agriculture
-          </p>
-          <hr className="border-gray-500 border-1 w-full mt-4" />
-        </div>
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 bg-gray-900 mb-4 mt-4 rounded-lg">
-          {blogs.map((blog) => (
-            <div key={blog._id}>
-              <Link to={`/viewblog/${blog._id}`}>
-                <div className="my-4">
-                  <PublicBlogCard
-                    title={blog.title}
-                    author={blog.author.firstName + " " + blog.author.lastName}
-                    authorID={blog.author._id}
-                    date={blog.createdAt}
-                    tags={blog.tags}
-                    likes={blog.likes.length}
-                    dislikes={blog.dislikes.length}
-                    comments={blog.comments.length}
-                    bookmarked={blog.bookmarked}
-                  />
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 bg-gray-900 mb-4 mt-4 rounded-lg">
+              {blogs.map((blog) => (
+                <div key={blog._id}>
+                  <Link to={`/viewblog/${blog._id}`}>
+                    <div className="my-4">
+                      <PublicBlogCard
+                        title={blog.title}
+                        author={blog.author.firstName + " " + blog.author.lastName}
+                        authorID={blog.author._id}
+                        date={blog.createdAt}
+                        tags={blog.tags}
+                        likes={blog.likes.length}
+                        dislikes={blog.dislikes.length}
+                        comments={blog.comments.length}
+                        bookmarked={blog.bookmarked}
+                      />
+                    </div>
+                  </Link>
                 </div>
-              </Link>
+              ))}
             </div>
-          ))}
-        </div>
+          </div>
       </div>
-    </div>
   );
-};
+}
 
-export default Home;
+export default Home
